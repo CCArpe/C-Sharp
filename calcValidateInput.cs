d5aef2f2b54aebc66bd73fa8ac7717731b164a36
CCArpe <37884429+CCArpe@users.noreply.github.com>
2021-05-19 14:07:18 -0400

    input validation added
    Updated calculator with inefficient, but basic input validation.

---
+++ calcValidateInput.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Boolean status = true;
+            int answer = 0;
+            int firstNum = 0;
+            int secondNum = 0;
+
+            while (status == true) {
+            Console.WriteLine("What type of operation would you like to do? Type add, subtract, multiply, or divide.");
+            String response = Console.ReadLine();
+        
+            if (response == ("add")) {
+                Console.WriteLine("Enter first number.");
+                firstNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second number.");
+                secondNum = Convert.ToInt32(Console.ReadLine());
+                answer = firstNum + secondNum;
+                Console.WriteLine("The answer is " + answer);
+            Console.WriteLine("Enter 1 to quit. Enter 2 for another operation.");
+            String input = Console.ReadLine();
+            if (input == "1") {
+                Console.WriteLine("Goodbye!");
+                status = false;
+            }
+            else if (input == "2"){
+                status = true;
+            }
+            }
+
+            else if (response == ("subtract")){
+                Console.WriteLine("Enter first number.");
+                firstNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second number.");
+                secondNum = Convert.ToInt32(Console.ReadLine());
+                answer = firstNum - secondNum;
+                Console.WriteLine("The answer is " + answer);
+            Console.WriteLine("Enter 1 to quit. Enter 2 for another operation.");
+            String input = Console.ReadLine();
+            if (input == "1") {
+                Console.WriteLine("Goodbye!");
+                status = false;
+            }
+            else if (input == "2"){
+                status = true;
+            }
+            }
+
+            else if (response == ("multiply")) {
+                Console.WriteLine("Enter first number.");
+                firstNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second number.");
+                secondNum = Convert.ToInt32(Console.ReadLine());
+                answer = firstNum * secondNum;
+                Console.WriteLine("The answer is " + answer);
+            Console.WriteLine("Enter 1 to quit. Enter 2 for another operation.");
+            String input = Console.ReadLine();
+            if (input == "1") {
+                Console.WriteLine("Goodbye!");
+                status = false;
+            }
+            else if (input == "2"){
+                status = true;
+            }
+            }
+
+            else if (response == ("divide")){
+                Console.WriteLine("Enter first number.");
+                firstNum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter second number.");
+                secondNum = Convert.ToInt32(Console.ReadLine());
+                answer = firstNum / secondNum;
+                Console.WriteLine("The answer is " + answer);
+            Console.WriteLine("Enter 1 to quit. Enter 2 for another operation.");
+            String input = Console.ReadLine();
+            if (input == "1") {
+                Console.WriteLine("Goodbye!");
+                status = false;
+            }
+            else if (input == "2"){
+                status = true;
+            }
+            }
+            else {
+                Console.WriteLine("Invalid input, please enter again!");
+            }
+
+
+            
+            }
+        }
+    }
+}
